d1d7e802d8326a9f635e558a1016d9b258b8814d
othon wallace <othonwallace123@gmail.com>
2025-02-15 23:45:46 -0300

    Desafio 1 - Inversão de String

--- reverse_string/src/lib.rs
+++ reverse_string/src/lib.rs
@@ -1,3 +1,7 @@
+pub fn reverse(input: &str) -> String {
+    input.chars().rev().collect()
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
@@ -10,5 +14,13 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_reverse() {
+        assert_eq!(reverse("Data Structures"), "serutcurtS ataD");
+        assert_eq!(reverse("Rust"), "tsuR");
+        assert_eq!(reverse(""), "");
+        assert_eq!(reverse("A"), "A");
     }
 }
